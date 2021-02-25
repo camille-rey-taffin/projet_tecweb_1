ec88862c9057c582d24dde4f05dc8544e709c7fa
camille rey <camille.rey.taffin@gmail.com>
2021-02-25 16:47:57 +0100

    modif procfile2

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn api_run:app
+web: gunicorn -c config/gunicorn.conf.py api_run:app
--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -1,3 +1,1 @@
 bind = '0.0.0.0:80'
-worker_class = 'gevent'  # not necessary
-timeout = 90  # not necessary
