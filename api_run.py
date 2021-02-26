587516592002b39145befad4eb0aeaef3a66ccf5
camille rey <camille.rey.taffin@gmail.com>
2021-02-26 10:23:59 +0100

    ajout traitement donnees et tentative identification

--- api_run.py
+++ api_run.py
@@ -1,30 +1,271 @@
-from flask import Flask, request
+# coding: utf-8
+
+from flask import Flask, request, json, jsonify, make_response, Response, redirect
 from flask_restful import Resource, Api
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import os
+import datetime
+import jwt
+import json
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloErtim(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-class Etudiants(Resource):
-    def get(self):
-        return {'type': 'etudiants'}
+#======================================================================
+# Gestion des identifiants des utilisateurs
+class User:
+    "Un utilisateur avec son id, son nom et son prénom"
+    def __init__(self, id:str, nom:str, prenom:str):
+        self.id = id
+        self.nom = nom
+        self.prenom = prenom
+
+    def to_json(self):
+        return {
+            "id":self.id,
+            "nom":self.nom,
+            "prenom":self.prenom
+            }
+
+
+def get_users(filename="users.json"):
+    """Crée les comptes utilisateur"""
+    with open(os.path.join("data", filename), "r", encoding="utf8") as data_file:
+        accounts = json.load(data_file)
+    users = []
+    for num, account in accounts.items():
+        user = User(account["id"], account["nom"], account["prenom"])
+        users.append(user)
+    return users
+
+users = get_users()
+
+
+# la clé (très) secrête
+key = "\xd5PE\xa3t\x96D\xa2\xae\xc2\xcfIq\xe7\xefk"
+
+
+
+def make_token(user):
+    """Génère le auth token"""
+    encode_params = {
+        "id": user.id,
+        "exp": datetime.datetime.utcnow()+\
+        datetime.timedelta(minutes=30)
+        }
+    try:
+        token = jwt.encode(encode_params, key, algorithm='HS256')
+        return token
+    except jwt.ExpiredSignatureError:
+        return "token expired!"
+
+
+def verify_user(nom:str, prenom:str):
+    """Vérifie que l'utilisateur est autorisé"""
+    for user in users:
+        if user.nom == nom and user.prenom == prenom:
+            return user
+        return False
+
+# =====================================================================
+
+
+def token_required(f):
+    """Crée le décorateur qui permettra de vérifier la présence
+    du token dans le header pour chaque requête"""
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+        if not token:
+            return {'message': 'token manquant'}
+        try:
+            data = jwt.decode(token, key)
+            current_user = data["id"]
+        except:
+            return {'message': 'token invalide'}
+        return f(current_user, *args, **kwargs)
+    return decorator
+
+
+
+#========================================================================
+
+file_data = "FR.txt"
+def get_data(filename=file_data):
+    """Retourne les données contenues dans notre fichier de données"""
+    data = []
+    with open(os.path.join("data", filename), "r", encoding="utf8") as data_file:
+        for line in data_file:
+            colonnes = line.strip().split('\t')
+            place = {}
+            place['geonameid'] = colonnes[0]
+            place['name'] = colonnes[1]
+            place['asciiname'] = colonnes[2]
+            place['alternatenames'] = colonnes[3]
+            place['latitude'] = colonnes[4]
+            place['longitude'] = colonnes[5]
+            place['feature class'] = colonnes[6]
+            place['feature code'] = colonnes[7]
+            place['country code'] = colonnes[8]
+            place['cc2'] = colonnes[9]
+            place['admin1 code'] = colonnes[10]
+            place['admin2 code'] = colonnes[11]
+            place['admin3 code'] = colonnes[12]
+            place['admin4 code'] = colonnes[13]
+            place['population'] = colonnes[14]
+            place['elevation'] = colonnes[15]
+            place['dem'] = colonnes[16]
+            place['timezone'] = colonnes[17]
+            place['modification date'] = colonnes[18]
+            data.append(place)
+    return data
+
+def save_data(data, filename=file_data):
+    """"Enregistre les nouvelles données dans notre fichier de données"""
+    with open(os.path.join("data", filename), "w", encoding="utf8") as data_file:
+        for place in data:
+            data_file.write(place['geonameid']+"\t"+place['name']+"\t"+place['asciiname']+"\t"+place['alternatenames']+"\t"+place['latitude']+"\t"+place['longitude']+"\t"+place['feature class']+"\t"+place['feature code']+"\t"+place['country code']+"\t"+place['cc2']+"\t"+place['admin1 code']+"\t"+place['admin2 code']+"\t"+place['admin3 code']+"\t"+place['admin4 code']+"\t"+place['population']+"\t"+place['elevation']+"\t"+place['dem']+"\t"+place['timezone']+"\t"+place['modification date'])
+
+
+#=========================================================================
+
+class Login(Resource):
+    """Login"""
     def post(self):
-        return {'hello': "je viens de créer un étudiant"}
-    def delete(self):
-        return {'hello': 'world'}
+        """"Crée le token d'un utilisateur si les informations reçues
+        correspondent bien à un compte utilisateur"""
 
-class CreateUserErtim(Resource):
-    def post(self, id_user):
-        data = request.json
-        return {'user_post': data, 'name': 'dddd'}
+        # On récupère les informations
+        info = request.json
 
-api.add_resource(HelloErtim, '/')
-api.add_resource(Etudiants, '/etudiants')
-api.add_resource(CreateUserErtim, '/user/<id_user>')
+        # On vérifie que les identifiants correpondent à un utilisateur
+        # Si oui on crée le token, l'utilisateur correspondant et le token sont renvoyés
+        # Si non, une erreur est renvoyé
+        user = verify_user(info["name"],info["firstname"])
+        if user:
+            token = make_token(user)
+            return {'User':user.to_json(), 'Token': token.decode('UTF-8')}
+        else:
+            return {"ERROR":"Username ou mot de passe incorrect"}, 400
+
+
+class Data(Resource):
+    """Gère la manipulation des données"""
+
+    @token_required
+    def get(self, current_user, asciiname=None):
+        """Gère l'accès et le filtrage des données"""
+
+        data = get_data()
+        result = []
+
+        # Renvoie toutes les données
+        if asciiname==None:
+            res = data, 200
+
+        # Renvoie les données filtrées sur l'asciiname
+        # Si aucune donnée n'est trouvée une erreur est renvoyée
+        elif asciiname!=None:
+            for place in data:
+                if place["asciiname"].lower() == asciiname.lower():
+                    result.append(place)
+            if len(result) == 0:
+                res = {"ERROR":"Aucun lieu n'a été trouvé"}, 404
+            else : res = result, 200
+
+
+        return res
+
+
+
+    @token_required
+    def put(self, current_user):
+        """Ajoute des données"""
+
+        # On va chercher les données déjà existantes
+        data = get_data()
+
+        # On reçoit les informations à ajouter
+        # envoyées par l'utilisateur
+        data_add = request.json
+
+        # On suppose que le geonameid est unique,
+        # on l'utilise donc pour identifier les éléments
+        # Si le geonameid existe déjà on renvoie une erreur,
+        # sinon on peut ajouter les nouvelles données
+        list_id = [d["geonameid"] for d in data]
+        if data_add["geonameid"] in list_id:
+            res = {"ERROR": "geonameid existe déjà"}, 400
+            return res
+        else:
+            time = datetime.datetime.utcnow()
+            data_add["modification date"] = str(time)
+            data.append(json.load(data_add))
+            save_data(data)
+            res = {"OK": "Lieu ajouté avec succès"}, 200
+            return res
+
+    @token_required
+    def post(self, current_user):
+        """Modifie des données"""
+
+        # On va chercher les données déjà existantes
+        data = get_data()
+
+        # On reçoit les informations à modifier
+        # envoyées par l'utilisateur
+        data_edit = request.json
+
+        # On recupère le geonameid pour connaître l'élément à modifier
+        geonameid = request.args.get('geonameid')
+
+        # On cherche l'élément correspondant au geonameid
+        # Si on le trouve alors on le modifie avec les informations reçues,
+        # Sinon on retourne un erreur
+        for n, d in enumerate(data):
+            if d["geonameid"] == geonameid:
+                for key, value in data_edit.items():
+                    d[key] = value
+                time = datetime.datetime.utcnow()
+                d["modification date"] = str(time)
+                save_data(data)
+                res = {"OK": "Données modifées avec succès"}, 200
+                return res
+        else :
+            res = {"ERROR": "Lieu non trouvé"}, 404
+
+
+    @token_required
+    def delete(self, current_user):
+        """Supprime des données"""
+
+        # On va chercher les données déjà existantes
+        data = get_data()
+
+        # On recupère le geonameid pour connaître l'élément à supprimer
+        geonameid = request.args.get('geonameid')
+
+        # On cherche l'élément correspondant au geonameid
+        # Si on le trouve alors on le supprime,
+        # Sinon on retourne un erreur
+        for n, d in enumerate(data):
+            if d["geonameid"] == geonameid:
+                del data[n]
+                save_data(data)
+                res = {"OK": "Lieu supprimé avec succés"}, 200
+                return res
+        else:
+            res = {"ERROR": "Lieu non trouvé"}, 404
+            return res
+
+
+api.add_resource(Login, "/", "/login")
+api.add_resource(Data, "/data")
 
 if __name__ == '__main__':
     app.run(debug=True)
