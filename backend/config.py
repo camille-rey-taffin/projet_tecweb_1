4af819284a29d6a02e72e865ad85aec4b91eaaaf
camille rey <camille.rey.taffin@gmail.com>
2021-02-27 20:12:03 +0100

    restructuration 3

--- backend/config.py
+++ backend/config.py
@@ -3,4 +3,4 @@ import os
 # Database initialization
 basedir = os.path.abspath(os.path.dirname(__file__))+"/.."
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'data/places.db')
-SECRET_KEY = "\xd5PE\xa3t\x96D\xa2\xae\xc2\xcfIq\xe7\xefk"
+SECRET_KEY = "\xf9'\xe4p(\xa9\x12\x1a!\x94\x8d\x1c\x99l\xc7\xb7e\xc7c\x86\x02MJ\xa0"
