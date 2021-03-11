77ccc938060e24345b20d0d46f5ca2c4ff96d17e
Camille Rey <camille.rey.taffin@gmail.com>
2021-03-11 15:40:15 +0100

    nettoyage code et ajout décorateur login

--- frontend/frontend.py
+++ frontend/frontend.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from flask import Flask, request, json, jsonify, request, Response, make_response, abort, redirect, render_template, url_for, session
 from flask_restful import Resource, Api
+from functools import wraps
 import json
 import requests
 
@@ -12,18 +13,15 @@ api = Api(app)
 # URL de notre application backend
 backend_api = "https://projet-tecweb-backend.herokuapp.com"
 
-
-def token_required_front(f):
-	"""Hg"""
+def login_required(f):
+	"""Décorateur pour les pages avec login requis. Permet de rrediriger vers la page de login puis vers la page sur laquelle on était."""
 	@wraps(f)
 	def decorator(*args, **kwargs):
 		if 'token' not in session:
-			reponse = redirect(url_for('.login', error="login_required", next="/data/add"))
+			reponse = redirect(url_for('.login', error="login_required", next = request.path))
 			return reponse
-
 		else :
-			current_user = decoded_token
-			return f(current_user, *args, **kwargs)
+			return f(*args, **kwargs)
 	return decorator
 
 def make_headers():
@@ -44,50 +42,45 @@ class Login(Resource):
 	def get(self):
 		"""Description """
 
-		error = False
-
-		if "error" in request.args:
-			error = request.args["error"]
+		error = False if "error" not in request.args else request.args["error"]
 
 		if error :
 			login_rend = render_template("login.html", login = False, fail = True, error = error)
+			resp = Response(login_rend, status=401, content_type="text/html")
 
 		elif 'token' in session:
 			login_rend = render_template("login.html", login = True, nom = session['user_name'], prenom = session['user_firstname'])
+			resp = Response(login_rend, status=200, content_type="text/html")
 
 		else :
 			login_rend = render_template("login.html", login = False)
-
-		resp = Response(login_rend, status=200, content_type="text/html")
+			resp = Response(login_rend, status=200, content_type="text/html")
 
 		return resp
 
 	def post(self):
 		"""Description"""
 		info_login = dict(request.form)
-		api_response = requests.post(backend_api + '/login', json = info_login, verify = False).json()
+		api_response = requests.post(backend_api + '/login', json = info_login, verify = False)
 
-		error = False
-		if "error" in request.args:
-			error = request.args["error"]
-
+		next_page = False if "next" not in request.args else request.args["next"]
 		# Si les identifiants sont corrects, on enregistre le token dans un cookie
 		# puis on redirige vers les données :
-		if "Token" in api_response:
+		if api_response.status_code == 200:
 			session.clear()
+			api_response = api_response.json()
 			session['token'] = api_response["Token"]
 			session['user_id'] = api_response['User']["id"]
 			session['user_name'] = api_response['User']["nom"]
 			session['user_firstname'] = api_response['User']["prenom"]
-			if error :
-				resp = redirect(request.args["next"])
+			if next_page:
+				resp = redirect(next_page)
 			else :
 				resp = redirect('/login')
 			return resp
 
 		# Si les identifiants sont incorrects :
-		elif api_response["status"] == "fail":
-			#render_response = render_template("login_error.html", message="Identifiant ou mot de passe incorrect !")
+		elif api_response.status_code == 401:
 			login_rend = render_template("login.html", login = False, fail = True, error = "wrong_id")
 			resp = Response(login_rend, status=401, content_type="text/html")
 			return resp
@@ -111,81 +104,83 @@ class Logout(Resource):
 class DataSearch(Resource):
 	"""Recherche de données"""
 
+	@login_required
 	def get(self):
 		"""Description """
 
 		filtres = dict(request.args)
 		filtres = {k: v for k, v in filtres.items() if v != ''}
 
-		if 'token' not in session:
-			reponse = redirect(url_for('.login', error="login_required", next="/data/search"))
+		api_response = requests.get(backend_api + "/data/search", headers = make_headers(), params = filtres, verify=False)
+
+		if api_response.status_code == 401 :
+			session.clear()
+			reponse = redirect(url_for('.login', error="invalid_token", next="/data/search"))
 			return reponse
 
+		if not filtres :
+			datasearch_rend = render_template("datasearch.html")
+			resp = Response(datasearch_rend, status=200, content_type="text/html")
+			return resp
+
 		else :
-			headers = { 'Authorization' : "Bearer " + session['token'] }
-
-			api_response = requests.get(backend_api + "/data/search", headers=headers, params = filtres, verify=False)
-
-			if api_response.status_code == 401 :
-				session.clear()
-				reponse = redirect(url_for('.login', error="invalid_token", next="/data/search"))
-				return reponse
-
-			if not filtres :
-				datasearch_rend = render_template("datasearch.html")
-				resp = Response(datasearch_rend, status=200, content_type="text/html")
-				return resp
-
-			else :
-				results = api_response.json()["results"]
-				datasearch_rend = render_template("datasearch.html", search = True, results = results)
-				resp = Response(datasearch_rend, status=200, content_type="text/html")
-				return resp
+			results = api_response.json()["results"]
+			datasearch_rend = render_template("datasearch.html", search = True, results = results)
+			resp = Response(datasearch_rend, status=200, content_type="text/html")
+			return resp
 
 class DataAdd(Resource):
 	"""Recherche de données"""
 
+	@login_required
 	def get(self):
 		"""Description """
 
-		if 'token' not in session:
-			reponse = redirect(url_for('.login', error="login_required", next="/data/add"))
-			return reponse
+		dataAdd_rend = render_template("dataAdd.html")
+		resp = Response(dataAdd_rend, status=200, content_type="text/html")
+		return resp
 
-		else :
-
-			dataAdd_rend = render_template("dataAdd.html")
-			resp = Response(dataAdd_rend, status=200, content_type="text/html")
-			return resp
-
+	@login_required
 	def post(self):
 		"""Description """
 
-		if 'token' not in session:
-			reponse = redirect(url_for('.login', error="login_required", next="/data/add"))
+		info_ajout = dict(request.form)
+		api_response = requests.put(backend_api + '/data/' + info_ajout['geonameid'], headers = make_headers(), json = info_ajout, verify = False)
+
+		if api_response.status_code == 401 :
+			session.clear()
+			resp = redirect(url_for('.login', error="invalid_token", next="/data/add"))
+			return resp
+
+		if api_response.status_code == 409 :
+			dataAdd_rend = render_template("dataAdd.html", geonameid_exists = info_ajout['geonameid'])
+			resp = Response(dataAdd_rend, status = 409, content_type="text/html")
+			return resp
+
+		if api_response.status_code == 200 :
+
+			dataAdd_rend = render_template("dataAdd.html", geonameid_created = info_ajout['geonameid'])
+			resp = Response(dataAdd_rend, status = 200, content_type="text/html")
+			return resp
+
+class Data(Resource):
+	"""Consultation, modification et suppression de données"""
+
+	@login_required
+	def get(self, geonameid):
+		"""Affichage"""
+
+		api_response = requests.get(backend_api + "/data/" + geonameid, headers = make_headers(), verify = False)
+
+		if api_response.status_code == 401 :
+			session.clear()
+			reponse = redirect(url_for('.login', error="invalid_token", next="/data/" + geonameid))
 			return reponse
 
-		else :
-
-			info_ajout = dict(request.form)
-			api_response = requests.put(backend_api + '/data/' + info_ajout['geonameid'], headers = make_headers(), json = info_ajout, verify = False)
-
-			if api_response.status_code == 401 :
-				session.clear()
-				reponse = redirect(url_for('.login', error="invalid_token", next="/data/add"))
-				return reponse
-
-			if api_response.status_code == 409 :
-				#reponse = redirect(url_for('.login', error="invalid_token", next="/data/search"))
-				dataAdd_rend = render_template("dataAdd.html", geonameid_exists = info_ajout['geonameid'])
-				resp = Response(dataAdd_rend, status = 409, content_type="text/html")
-				return resp
-
-			if api_response.status_code == 200 :
-
-				dataAdd_rend = render_template("dataAdd.html", geonameid_created = info_ajout['geonameid'])
-				resp = Response(dataAdd_rend, status=200, content_type="text/html")
-				return resp
+		result = api_response.json()['data']
+		data_rend = render_template("data.html", search = True, result = result)
+		resp = Response(data_rend, status=200, content_type="text/html")
+		return resp
 
 
 api.add_resource(Index, "/", "/index")
@@ -193,6 +188,7 @@ api.add_resource(Login, "/login")
 api.add_resource(Logout, "/logout")
 api.add_resource(DataSearch, "/data/search")
 api.add_resource(DataAdd, "/data/add")
+api.add_resource(Data, "/data/<geonameid>")
 
 
 if __name__ == '__main__':
